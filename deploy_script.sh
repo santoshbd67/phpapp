0637ba1c93769fa0c687e4af26c41b52e4fcc2c4
santoshbd67 <150413783+santoshbd67@users.noreply.github.com>
2024-02-24 18:22:04 +0530

    Update deploy_script.sh

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-aws ecr get-login-password --region ${AWS_DEFAULT_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
-docker stop ${IMAGE_REPO_NAME} || true
-docker rm ${IMAGE_REPO_NAME} || true
-docker pull ${REPOSITORY_URI}:${IMAGE_TAG}
-docker run -d -p 80:80 --name ${IMAGE_REPO_NAME} ${REPOSITORY_URI}:${IMAGE_TAG}
+sudo docker pull ${REPOSITORY_URI}:${IMAGE_TAG}
+sudo docker stop ${IMAGE_REPO_NAME} || true
+sudo docker rm ${IMAGE_REPO_NAME} || true
+sudo docker run -d -p 8080:80 --name ${IMAGE_REPO_NAME} ${REPOSITORY_URI}:${IMAGE_TAG}
